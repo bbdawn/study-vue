8f01406846d2c3c2e2b6d60f872147fc37eebcdb
bbdawn <jh.lee2@okestro.com>
2022-12-01 16:07:47 +0900

    Vue 반복문 v-for

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
+
+  <div class="menu">
+    <!-- v-for="작명 in 몇회" -->
+    <!-- :key=""의 용도 => 반복문 쓸 때 꼭 써야함. 반복문 돌린 요소를 컴퓨터가 구분하기 위해 씀  -->
+    <a v-for="작명 in menu" :key="작명">{{작명}}</a>
+
+    <br>
+    <!-- Vue 반복문은 2개까지 가능. 왼쪽 변수는 array 내의 데이터, 오른쪽 변수는 1씩 증가하는 정수  -->
+    <a v-for="(a,i) in menu" :key="i">{{a}}</a>
+  </div>
+
+
+
   <img alt="Vue logo" src="./assets/logo.png">
-  <div>
+  <!-- <div>
     <h4 class="red" :style="스타일">{{products[0]}}</h4>
     <p>{{ price1 }}만원 </p>
   </div>
@@ -11,7 +24,12 @@
    <div>
     <h4 class="red" :style="스타일">{{products[2]}}</h4>
     <p>{{ price3 }}만원 </p>
+  </div> -->
+
+   <div v-for="(a,i) in products" :key="i">
+    <h4 class="red" :style="스타일">{{products[i]}}</h4>
   </div>
+
 </template>
 
 <script>
@@ -24,7 +42,8 @@ export default {
       price2 : 60,
       price3 : 70,
       스타일 : 'color : blue',
-      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      menu : ['Home', 'Shop', 'About'],
     }
   },
   components: {
@@ -42,4 +61,15 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
 </style>
