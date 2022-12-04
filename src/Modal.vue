3fbe8e452da6803e19d884fe79f783e7ae12712f
bbdawn <jh.lee2@okestro.com>
2022-12-04 17:22:24 +0900

    input

--- src/Modal.vue
+++ src/Modal.vue
@@ -6,7 +6,11 @@
       <h4>{{원룸들[누른거].title}}</h4> 
        <img :src="원룸들[누른거].image" >
       <p>{{원룸들[누른거].content}}</p> 
-      <input @input="month = $event.target.value">
+
+      <!-- input 방법1 -->
+      <!-- <input @input="month = $event.target.value"> -->
+      <!-- input 방법2 -->
+      <input v-model="month">
       <p>{{month}}개월 선택함 : {{원룸들[누른거].price * month}}원</p>
       <!-- <button @click="모달창열렸니 = false">닫기</button> -->
    <button @click="$emit('closeModal')">닫기</button>
